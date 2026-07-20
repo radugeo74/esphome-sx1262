5c4ce365fa868f3398f3b32ec26d565323d68b93
radugeo74 <94635143+radugeo74@users.noreply.github.com>
2026-07-20 16:12:26 +0300

    Update __init__.py

--- components/wmbus_common/__init__.py
+++ components/wmbus_common/__init__.py
@@ -1,12 +1,10 @@
+import esphome.config_validation as cv
+from esphome.const import SOURCE_FILE_EXTENSIONS, CONF_ID
+from esphome.loader import get_component, ComponentManifest
+from esphome import codegen as cg
 from pathlib import Path
 
-import esphome.codegen as cg
-import esphome.config_validation as cv
-from esphome.const import CONF_ID
-
-
 CODEOWNERS = ["@SzczepanLeon", "@kubasaw"]
-
 CONF_DRIVERS = "drivers"
 
 wmbus_common_ns = cg.esphome_ns.namespace("wmbus_common")
@@ -14,21 +12,15 @@ WMBusCommon = wmbus_common_ns.class_("WMBusCommon", cg.Component)
 
 
 AVAILABLE_DRIVERS = {
-    file.stem.removeprefix("driver_")
-    for file in Path(__file__).parent.glob("driver_*.cpp")
+    f.stem.removeprefix("driver_") for f in Path(__file__).parent.glob("driver_*.cpp")
 }
 
 _registered_drivers = set()
 
 
-def validate_driver(driver):
-    _registered_drivers.add(driver)
-    return driver
-
-
 validate_driver = cv.All(
     cv.one_of(*AVAILABLE_DRIVERS, lower=True, space="_"),
-    validate_driver,
+    lambda driver: _registered_drivers.add(driver) or driver,
 )
 
 
@@ -36,19 +28,30 @@ CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(WMBusCommon),
         cv.Optional(CONF_DRIVERS, default=set()): cv.All(
-            lambda value: AVAILABLE_DRIVERS if value == "all" else value,
+            lambda x: AVAILABLE_DRIVERS if x == "all" else x,
             {validate_driver},
         ),
     }
 )
 
 
-def FILTER_SOURCE_FILES():
-    """Exclude meter drivers not selected in the YAML configuration."""
-    unused_drivers = AVAILABLE_DRIVERS - _registered_drivers
-    return {f"driver_{driver}.cpp" for driver in unused_drivers}
+class WMBusComponentManifest(ComponentManifest):
+    exclude_drivers: set[str]
+
+    @property
+    def resources(self):
+        exclude_files = {f"driver_{name}.cpp" for name in self.exclude_drivers}
+        SOURCE_FILE_EXTENSIONS.add(".cpp")
+        resources = [fr for fr in super(
+        ).resources if fr.resource not in exclude_files]
+        SOURCE_FILE_EXTENSIONS.discard(".cpp")
+        return resources
 
 
 async def to_code(config):
+    component = get_component("wmbus_common")
+    component.__class__ = WMBusComponentManifest
+    component.exclude_drivers = AVAILABLE_DRIVERS - _registered_drivers
+
     var = cg.new_Pvariable(config[CONF_ID], sorted(_registered_drivers))
     await cg.register_component(var, config)
